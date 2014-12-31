e6539c16698fb83081f656b8a707566f07038c74
Fred Leitz <fleitz@atimi.com>
2014-12-31 14:31:30 -0800

    Initial Commit

---
+++ TickerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TickerTest
+//
+//  Created by Fred Leitz on 2014-12-31.
+//  Copyright (c) 2014 Fred Leitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
