a7b917243c1e47d4d7a52ab8094ceb987bf4c143
Fred Leitz <fleitz@atimi.com>
2014-12-31 14:37:20 -0800

    add files

--- TickerTest/ViewController.swift
+++ TickerTest/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var label = UILabel(frame: CGRectMake(0, 0, 3200, 100))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var str = "A REALLY LONG PIECE OF TEXT A REALLY LONG PIECE OF TEXT A REALLY LONG PIECE OF TEXT A REALLY LONG PIECE OF TEXT A REALLY LONG PIECE OF TEXT A REALLY LONG PIECE OF TEXT A REALLY LONG PIECE OF TEXT "
+        label.text = "\(str) \(str) \(str) \(str) \(str) \(str) \(str) \(str) \(str)"
+   
+        view.addSubview(label)
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animateWithDuration(15.0){
+            self.label.frame.origin.x = -3200
+        }
     }
 
     override func didReceiveMemoryWarning() {
